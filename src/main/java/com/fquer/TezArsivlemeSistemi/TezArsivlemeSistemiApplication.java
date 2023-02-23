4ba7e9779a8e44d7fe7b6ea260caeeb247ca7cb5
Kaan Bilgi <54486031+fquer@users.noreply.github.com>
2023-02-23 21:55:45 +0300

    init

---
+++ src/main/java/com/fquer/TezArsivlemeSistemi/TezArsivlemeSistemiApplication.java
@@ -0,0 +1,13 @@
+package com.fquer.TezArsivlemeSistemi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TezArsivlemeSistemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TezArsivlemeSistemiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fquer/TezArsivlemeSistemi/TezArsivlemeSistemiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fquer.TezArsivlemeSistemi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TezArsivlemeSistemiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
